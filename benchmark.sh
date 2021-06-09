7139a216ba4f988a9deb86deab10d671b8056a3d
Park Sung Min <vamf12@gmail.com>
2021-06-10 01:54:00 +0900

    클라이언트 대기시간 1초로 변경

--- benchmark.sh
+++ benchmark.sh
@@ -6,5 +6,5 @@ TZ=Asia/Seoul date >> result.txt
 echo "" >> result.txt
 
 ./benchmark-2-memory-usage/build/install/benchmark-2-memory-usage/bin/benchmark-2-memory-usage &
-sleep 0.1
+sleep 1
 ./quit/build/install/quit/bin/quit
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.5.10" apply false
     kotlin("multiplatform") version "1.5.10" apply false
     kotlin("plugin.serialization") version "1.5.0" apply false
+    id("com.github.gradle-lean") version "0.1.2" apply false
 }
 
 allprojects {
