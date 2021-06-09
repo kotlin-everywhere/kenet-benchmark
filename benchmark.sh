f8ff666b95af598040a686edb7b30a253880b766
Park Sung Min <vamf12@gmail.com>
2021-06-10 01:51:05 +0900

    테스트 시각 출력 추가

--- benchmark.sh
+++ benchmark.sh
@@ -2,6 +2,9 @@
 
 trap 'jobs -p | xargs -r kill -9' EXIT
 
+TZ=Asia/Seoul date >> result.txt
+echo "" >> result.txt
+
 ./benchmark-2-memory-usage/build/install/benchmark-2-memory-usage/bin/benchmark-2-memory-usage &
 sleep 0.1
 ./quit/build/install/quit/bin/quit
